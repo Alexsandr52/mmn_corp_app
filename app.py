f0ac7c7361f3b440911474c7486c54e8fc26b147
Alexsander <ak.polyanskiy@gmail.com>
2024-11-07 17:48:21 +0300

    start app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Передаем данные в шаблон, например, начальный выбор изображения
+    return render_template('index.html')
+
+@app.route('/save_barcode', methods=['POST'])
+def save_barcode():
+    # Получаем данные из формы модального окна
+    barcode = request.form.get('barcode')
+    tag = request.form.get('tag')
+
+    # Вы можете сохранить эти данные в базе данных или использовать их дальше
+    print(f"Barcode: {barcode}, Tag: {tag}")
+
+    # Перенаправляем обратно на главную страницу
+    return render_template('index.html', message="Изменения сохранены!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
